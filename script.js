9fa50b3f8d1a5d1d0543809c34926e4125747136
Zainabibb <156958010+Zainabibb@users.noreply.github.com>
2024-01-30 14:38:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+// SpeakHello.js
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+(function(window) {
+  // STEP 3: Create an object, called 'helloSpeaker'
+  var helloSpeaker = {};
+
+  // STEP 4: Attach the 'speak' method to 'helloSpeaker' object
+  helloSpeaker.speak = function(name) {
+    var speakWord = "Hello";
+    console.log(speakWord + " " + name);
+  };
+
+  // STEP 5: Expose the 'helloSpeaker' object to the global scope
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+// SpeakGoodBye.js
+// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+(function(window) {
+  // STEP 7: Create an object, called 'byeSpeaker'
+  var byeSpeaker = {};
+
+  // STEP 8: Attach the 'speak' method to 'byeSpeaker' object
+  byeSpeaker.speak = function(name) {
+    var speakWord = "Good Bye";
+    console.log(speakWord + " " + name);
+  };
+
+  // STEP 9: Expose the 'byeSpeaker' object to the global scope
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
 
